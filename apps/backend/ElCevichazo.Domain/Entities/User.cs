d2dace7625fc2a3ccb30072c5c2c79d034b5edb6
victoriaBuenx <victoriabuenomijares@gmail.com>
2026-09-24 22:58:06 -0600

    fix(domain): resolve duplicate user entity path

--- apps/backend/ElCevichazo.Domain/Entities/User.cs
+++ apps/backend/ElCevichazo.Domain/Entities/User.cs
@@ -1,11 +1,21 @@
+using System.ComponentModel.DataAnnotations;
 using ElCevichazo.Domain.Enums;
 namespace ElCevichazo.Domain.Entities;
 
 public class User
 {
     public Guid Id {get ; set;}
+    
+    [Required]
+    [MaxLength(100)]
     public string UserName {get; set;} = string.Empty;
+
+    [Required]
+    [MaxLength(250)]
+    [EmailAddress]
     public string Email {get; set;} = string.Empty;
+
+    [Required]
     public string PasswordHash {get; set;} = string.Empty;
     public bool IsActive {get; set;} = true;
     public DateTime CreatedAt {get; set;} = DateTime.UtcNow;
